20b359d59b65d867ee8b3372e9666a250d8e5448
MagpieTaxi <106232576+MagpieTaxi@users.noreply.github.com>
2025-11-01 11:04:03 -0400

    add progression (1)

---
+++ mods/recipes/init.lua
@@ -0,0 +1,7 @@
+core.register_craft({
+	output = "tools:rudimentary_mattock",
+	recipe = {
+		{"blocks:pebble","blocks:pebble"},
+		{"blocks:stick","items:plant_fiber"}
+	}
+})
--- mods/tools/init.lua
+++ mods/tools/init.lua
@@ -1,3 +1,16 @@
 core.register_tool("tools:rudimentary_mattock",{
-	
+	description = "Rudimentary Mattock",
+	inventory_image = "rudimentary_mattock.png",
+    wield_image = "rudimentary_mattock.png",
+    range = 4.0,                     -- default reach distance
+    tool_capabilities = {
+        full_punch_interval = 1.0,
+        max_drop_level = 1,
+        groupcaps = {
+            harvestable_pickaxe = {times={[1]=1.0,[2]=5.0,[3]=8.0,[4]=12.0}, uses=48, maxlevel=1},
+            harvestable_axe = {times={[1]=1.0,[2]=5.0,[3]=8.0,[4]=12.0}, uses=48, maxlevel=1},
+			harvestable_shovel = {times={[1]=1.0,[2]=2.0,[3]=5.0,[4]=12.0},uses=48,maxlevel=1}
+        },
+        damage_groups = {harvestable_pickaxe = 1,harvestable_axe = 1},
+    },
 })
